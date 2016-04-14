54e786fd448bed92eb65eb4532046eb82aa2c844
Kershan Naidoo <kershan@immedia.co.za>
2016-04-14 09:18:21 +0200

    Implement basic user profile

--- app.js
+++ app.js
@@ -4,12 +4,28 @@ var builder = require('botbuilder');
 // Create bot and add dialogs
 var bot = new builder.BotConnectorBot({ appId: 'YourAppId', appSecret: 'YourAppSecret' });
 bot.add('/', function (session) {
-    session.send('Hello World');
+    // Request user's name if not already known to the bot
+    if (!session.userData.name) {
+        session.beginDialog('/profile');
+    } else {
+        session.send('Hello %s!', session.userData.name);
+    }
 });
+
+// Setup profile dialog
+bot.add('/profile', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
 
 // Setup Restify Server
 var server = restify.createServer();
 server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
 server.listen(process.env.port || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url); 
+    console.log('%s listening to %s', server.name, server.url);
 });
